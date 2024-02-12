463c39f95cb380f5a94fb37c6069834ac3e24c5a
illiteratewriter <manukoshyabraham@gmail.com>
2024-02-12 12:53:55 +0530

    chore: cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 pub mod dom;
 pub mod html;
 
-use std::collections::HashMap;
 use termtree::Tree;
 
 fn tree_maker(n: &dom::Node) -> Tree<String> {
@@ -27,12 +26,8 @@ fn tree_maker(n: &dom::Node) -> Tree<String> {
 }
 
 fn main() {
-    // let h1 = dom::text(String::from("h1"));
-    // let h2 = dom::text(String::from("h2"));
-    // let div = dom::elem(String::from("div"), HashMap::new(), vec![h2]);
-    // let node = dom::elem(String::from("div"), HashMap::new(), vec![h1, div]);
-
-    let s = String::from("qq<html>
+    let s = String::from(
+        "qq<html>
     <head>
       <title>Test</title>
     </head>
@@ -42,7 +37,8 @@ fn main() {
       <p class=\"inner\" id=\"bye\">Goodbye!</p>
     </div>
   </html>
-  ");
+  ",
+    );
 
     let node = html::parse(s);
 
